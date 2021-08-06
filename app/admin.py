c290c9fe6d761d72e2353b1464c260c62ba8096c
sajith <iamsajithsasi@gmail.com>
2021-08-06 23:59:45 +0530

    code update

--- app/admin.py
+++ app/admin.py
@@ -1,7 +1,34 @@
 from django.contrib import admin
 from .models import SampleModel
+
+@admin.display(empty_value='NA')
 class SampleModalAdmin(admin.ModelAdmin):
-    list_display = ('title', 'info')
+    list_display = ('title', 'info', 'year')
+    # list_display_links = ('info',)
+    list_filter = ('title', 'year')
+    
+    search_fields = ['title', 'description']
+    
+    # empty_value_display = 'NA'
+    
+    # fields = (('title', 'description'), 'year')
+    
+    fieldsets = (
+        (None, {
+            'fields': ('title', 'description',)
+        }),
+        ('Other', {
+            'classes': ('wide', 'extrapretty'),
+            # 'classes': ('collapse'),
+            'fields': ('year',),
+        }),
+    )
+
+    # formfield_overrides = { SampleModel.description: {'widget': RichTextEditorWidget},}
+
+    # exclude = ('year',)
+
+    
     def info(self, obj):
         return obj.description
 
--- app/models.py
+++ app/models.py
@@ -5,6 +5,7 @@ class SampleModel(models.Model):
 
     title = models.CharField(max_length=100, default='', null=False)
     description = models.TextField(null=True)
+    year = models.DateField(blank=True, null=True)
 
     # for showing title in the admin tabs
     def __str__(self):
